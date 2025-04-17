7d6c040e6aec632c7d2950556426594a833f6b0f
João Vitório <141413107+TheJoaoVitorio@users.noreply.github.com>
2025-04-17 20:24:08 -0300

    Refactor: Validation to know if you have uploaded the ENV

--- tube/lib/config/env_config.dart
+++ tube/lib/config/env_config.dart
@@ -1,6 +1,15 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class EnvConfig{
-  static String get apiUrl => dotenv.env['API_URL'] ?? '';
-  static String get apiKey => dotenv.env['API_KEY'] ?? '';
+class EnvConfig {
+  static String get apiUrl {
+    final url = dotenv.env['API_URL'];
+    assert(url != null && url.isNotEmpty, 'API_URL não carregada!');
+    return url!;
+  }
+
+  static String get apiKey {
+    final key = dotenv.env['API_KEY'];
+    assert(key != null && key.isNotEmpty, 'API_KEY não carregada!');
+    return key!;
+  }
 }
