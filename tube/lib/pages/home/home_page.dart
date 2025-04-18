80c382bebae22aa00719d421fa6cf181dc53d69b
João Vitório <141413107+TheJoaoVitorio@users.noreply.github.com>
2025-04-18 20:25:55 -0300

    Feat: created HomePage

---
+++ tube/lib/pages/home/home_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:tube/components/carousel_card/carousel_card.dart';
+import 'package:tube/models/video_model/video_model.dart';
+import 'package:tube/services/api_youtube/api_youtube.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final ApiYoutubeService apiYoutubeService = ApiYoutubeService();
+
+  List<VideoModel> _videos = [];
+  bool _isLoading = true;
+  String _errorMessage = '';
+
+  @override
+  void initState(){
+    super.initState();
+    _fetchTrailers();
+  }
+
+  @override
+  void dispose() {
+    _videos.clear(); 
+    super.dispose();
+  }
+
+  Future<void>_fetchTrailers() async {
+    try{
+      final resultFetch = await apiYoutubeService.getTrailersFilms();
+      setState(() {
+        _videos = resultFetch;
+        _isLoading = false;
+      });
+    }catch(e){
+      setState(() {
+        _errorMessage = e.toString();
+        _isLoading = false;
+      });
+    }
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+
+    if(_isLoading){
+      return Center(child: CircularProgressIndicator(),);
+    }
+
+    if(_errorMessage.isNotEmpty){
+      return Center(child: Text('Erro: $_errorMessage'),);
+    }
+
+    return ListView(
+      children: [
+        CarouselCard(videos: _videos),
+        SizedBox(height: 20,),
+        Text('teste')
+      ],
+    );
+  }
+}
