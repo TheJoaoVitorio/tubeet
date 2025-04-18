b3a9f96a3008b17eaaf8b6d4e7cbe59b0e668b7d
João Vitório <141413107+TheJoaoVitorio@users.noreply.github.com>
2025-04-18 17:10:19 -0300

    Update: declaration HomeContainerPage

--- tube/lib/main.dart
+++ tube/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
           googleFontBuilder: GoogleFonts.oswald,
         ),
       ),
-      home: const HomePage(),
+      home: const HomeContainerPage(),
     );
   }
 }
