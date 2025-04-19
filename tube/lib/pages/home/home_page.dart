06e0aed7d68804187ceb182a97a64fadb42369a9
João Vitório <141413107+TheJoaoVitorio@users.noreply.github.com>
2025-04-18 21:17:00 -0300

    Update:  HomePage structure organized by components

--- tube/lib/pages/home/home_page.dart
+++ tube/lib/pages/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tube/components/carousel_card/carousel_card.dart';
+import 'package:tube/components/carousel_circle_card/carousel_circle_card.dart';
+import 'package:tube/components/listtile_header/listtile_header.dart';
 import 'package:tube/models/video_model/video_model.dart';
 import 'package:tube/services/api_youtube/api_youtube.dart';
 
@@ -11,7 +13,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   final ApiYoutubeService apiYoutubeService = ApiYoutubeService();
 
   List<VideoModel> _videos = [];
@@ -19,48 +20,50 @@ class _HomePageState extends State<HomePage> {
   String _errorMessage = '';
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _fetchTrailers();
   }
 
   @override
   void dispose() {
-    _videos.clear(); 
+    _videos.clear();
     super.dispose();
   }
 
-  Future<void>_fetchTrailers() async {
-    try{
+  Future<void> _fetchTrailers() async {
+    try {
       final resultFetch = await apiYoutubeService.getTrailersFilms();
       setState(() {
         _videos = resultFetch;
         _isLoading = false;
       });
-    }catch(e){
+    } catch (e) {
       setState(() {
         _errorMessage = e.toString();
         _isLoading = false;
       });
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
-
-    if(_isLoading){
-      return Center(child: CircularProgressIndicator(),);
+    if (_isLoading) {
+      return Center(child: CircularProgressIndicator());
     }
 
-    if(_errorMessage.isNotEmpty){
-      return Center(child: Text('Erro: $_errorMessage'),);
+    if (_errorMessage.isNotEmpty) {
+      return Center(child: Text('Erro: $_errorMessage'));
     }
 
     return ListView(
+      padding: EdgeInsets.all(18),
       children: [
+        ListTitleHeader(titleHeader: 'Filmes', iconLeading: Icons.movie),
         CarouselCard(videos: _videos),
-        SizedBox(height: 20,),
-        Text('teste')
+        SizedBox(height: 20),
+        ListTitleHeader(titleHeader: 'Canais', iconLeading: Icons.subscriptions),
+        CarouselCircleCard(),
       ],
     );
   }
